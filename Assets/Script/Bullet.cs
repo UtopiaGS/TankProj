d2c941fcb5e0ab0ea654128a73506bd2b8a5887a
ErikCruz <erikcruzdev@gmail.com>
2025-10-24 19:14:56 -0300

    back

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,19 +1,12 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
     public float speed;
-    Rigidbody rb;
-    void Start()
-    {
-        rb= GetComponent<Rigidbody>();
-    }
-
   
+
     void LateUpdate()
     {
-        Vector3 moveDirection = transform.forward * speed;
-        rb.MovePosition(rb.position + moveDirection * Time.fixedDeltaTime);
+        transform.position += new Vector3(0,0,speed * Time.deltaTime);
     }
 }
